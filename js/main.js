82f784433a036ef243a138a77ea9274d4a08e43b
menth0s <marslite@pm.me>
2023-06-15 16:49:49 -0400

    shaping Lucky Safari HTML & CSS structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 console.log('hello, it works!');
+
+
+  /*----- constants -----*/
+  const combos ={
+    '0' : 'crocodile',
+    '1' : 'lion',
+    '2': 'gazelle',
+    '3': 'hippo',
+    '4': 'parrot'
+  };
+
+
+  /*----- state variables -----*/
+  let board;
+  let spin;
+  let winCash; 
+
+
+  /*----- cached elements  -----*/
+
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
+
+function init(){
+
+}
